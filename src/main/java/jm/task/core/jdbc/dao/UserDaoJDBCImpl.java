d035a87369703c885ed63f7ea6dec2049ca27b16
Maksim M0rokov <maxmorokov7@gmail.com>
2024-01-26 12:03:48 +0300

    Практическая задача 1.1.4 Java pre-project. Практическая задача 1.1.2. Работа с базой данных c помощью JDBC. Мороков Максим

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,101 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import static jm.task.core.jdbc.util.Util.getConnection;
+
+
 public class UserDaoJDBCImpl implements UserDao {
+
+    private Connection connection = getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS users(id INT PRIMARY KEY AUTO_INCREMENT," +
+                    "name VARCHAR(255), " +
+                    "lastname VARCHAR(255)," +
+                    "age TINYINT)");
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    //”даление таблицы User(ов) Ц не должно приводить к исключению, если таблицы не существует
     public void dropUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE IF EXISTS users");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
 
+        String query = "INSERT INTO users(name, lastname, age) VALUES(?,?,?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setInt(3, age);
+            preparedStatement.executeUpdate();
+            System.out.printf("User с именем Ц %s добавлен в базу данных \n", name);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
+    //”даление User из таблицы ( по id )
     public void removeUserById(long id) {
-
+        String query = "DELETE FROM users WHERE id=?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+
+    //ѕолучение всех User(ов) из таблицы
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong(1));
+                user.setName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge(resultSet.getByte(4));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
+    //ќчистка содержани€ таблицы
     public void cleanUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("TRUNCATE TABLE users");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/users";
+    private final static String PASSWORD = "root";
+    private final static String USER = "root";
+
+
+    //    Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("We are connected!");
+                connection.setAutoCommit(false);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
